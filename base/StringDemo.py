e069c20d5eff5b82e1a0f0dd158e75b7df893fcb
gyoomi <gyoomi0709@foxmail.com>
2018-08-22 18:13:05 +0800

    str learning

---
+++ base/StringDemo.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Author  : Leon
+# @Version : 2018/8/22 17:00
+
+string = "hello world"
+"""
+print[string[0]]
+print[string[1]]
+print[string[19]]
+"""
+
+# 切片是指对操作的对象截取其中一部分的操作。字符串、列表、元组都支持切片操作
+abc = "abcdefg"
+# print(abc[0:3])
+# 实现反转字符的效果
+# print(abc[::-1])
+# print(abc[2::])
+# print(abc[0:3:2])
+# 截取前3个
+# print(abc[:3])
+# print(abc[1:])
+# print(abc[::-2])
+
+mystr = 'hello world itcast and itcastcpp'
+# find
+# print(mystr.find("itcast", 0, 10))
+# print(mystr.find("o", 0))
+# index
+# print(mystr.index("l", 0, 1))
+# print(mystr.index("o", 0))
+# count
+# print(mystr.count("o", 5, 6))
+# print(mystr.count("haha"))
+# replace
+# testStr = "my world ha ha h"
+# testStr = testStr.replace("ha", "HA", 1)
+# print(testStr)
+test = "Line1-abcdef \nLine2-abc \nLine4-abcd"
+# 分隔符，默认为所有的空字符，包括空格、换行(\n)、制表符(\t)等
+# print(test.split())
+# print(test.split(" ", 1))
+# capitalize 将第一个单词变大写，其余变小写
+# print("aBCDf".capitalize())
+# test2 = "哈哈我是BACdfF"
+# test2 = test2.capitalize()
+# print(test2)
+
+
+
+
+
+
+
+
+
+
+
