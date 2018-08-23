6c83745babfa7f0207c8f9fd0d7eb9534310809f
gyoomi <gyoomi0709@foxmail.com>
2018-08-23 18:12:02 +0800

    list learning

---
+++ base/ListDemo.py
@@ -0,0 +1,102 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Author  : Leon
+# @Version : 2018/8/23 9:50
+
+
+
+"""
+for name in namesList:
+    print(name)
+"""
+"""
+i = 0
+while i < len(namesList):
+    print(namesList[i])
+    i += 1
+"""
+
+# 添加元素
+# append, extend, insert
+
+# namesList = ['xiaoWang', 'xiaoZhang', 'xiaoHua']
+# append()
+# namesList.append(True)
+# namesList.append(0)
+# print(namesList)
+
+# extend()
+# 在列表末尾一次性追加另一个序列中的多个值（用新列表扩展原来的列表）
+# aList = [123, 'xyz', 'zara', 'abc', 123];
+# bList = [2009, 'manni'];
+# aList.extend(bList)
+# print(aList)
+
+# insert(index, object) 在指定位置index前插入元素object
+
+# 修改元素("改")
+# 查找元素("查"in, not in, index, count)
+# #############################################################
+# in（存在）,如果存在那么结果为true，否则为false
+# not in（不存在），如果不存在那么结果为true，否则false
+"""
+findName = input("请输入你要查询的姓名：")
+if findName not in namesList:
+    print("对不起，你所查询的姓名在系统中不存在")
+else:
+    print("哈哈")
+"""
+
+"""
+if findName in namesList:
+    print("结果：" + findName)
+else:
+    print("对不起，你所查询的姓名不存在")
+"""
+# ################################################
+# index, count
+# index和count与字符串中的用法相同
+# namesList = ['xiaoWang', 'xiaoZhang', 'xiaoHua', "xiaoMing", "xiaoMing"]
+# print(namesList.index("xiaoMing", 4))
+
+# ##############################################
+# 删除元素("删"del, pop, remove)
+# del：根据下标进行删除
+# pop：删除最后一个元素
+# remove：根据元素的值进行删除
+
+# namesList = ['xiaoWang', 'xiaoZhang', 'xiaoHua', "xiaoMing", "xiaoMing"]
+# del namesList[3]
+# print(namesList)
+
+# 要移除列表元素的索引值，不能超过列表总长度，默认为 index=-1，删除最后一个列表值。
+# namesList = ['xiaoWang', 'xiaoZhang', 'xiaoHua', "xiaoMing"]
+# namesList.pop(-2)
+# namesList.pop()
+# print(namesList)
+
+# remove() 函数用于移除列表中某个值的第一个匹配项；没有返回值
+namesList = ['xiaoWang', 'xiaoZhang', 'xiaoHua', "xiaoMing"]
+namesList.remove("xiaoZhang")
+print(namesList)
+
+# 排序(sort, reverse)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
