40ca9b630a139ed9e13a6f232151242856c48568
gyoomi <gyoomi0709@foxmail.com>
2018-08-24 23:22:14 +0800

    函数

---
+++ base/FunDemo.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Author  : Leon
+# @Version : 2018/8/24 22:58
+# def printInfo():
+#     print("-" * 20)
+#     print("---人生苦短，我用python---")
+#     print("-" * 20)
+#
+# printInfo()
+
+# def test(a, b):
+#     print("%d" % (a + b))
+# # test(11, 22)
+# help(test)
+
+
+def add2num(a, b):
+    return a + b
+
+
+result = add2num(98, 234)
+print(result)
+
+
+
+
+
+
